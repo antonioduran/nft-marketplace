eda06332ea62244ae939f2d02f7182ed6dd9305c
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-07-25 23:48:40 -0500

    [ Adding component - BaseLayout ]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Navbar } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   )
-  
-  
 }
 
 export default MyApp
