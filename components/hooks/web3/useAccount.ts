779b29a4936797cd21b21d379c69fd328e5c6a33
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-10-23 17:24:26 -0500

    [ Handle accounts changed - Mutate account ]

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,17 +1,61 @@
-import useSWR from "swr";
 import { CryptoHookFactory } from "@_types/hooks";
+import { useEffect } from "react";
+import useSWR from "swr";
 
-type AccountHookFactory = CryptoHookFactory<string, string>
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>
+
+type UseAccountResponse = {
+  connect: () => void
+}
+
 export type UseAccountHook = ReturnType<AccountHookFactory>
 
-export const hookFactory : AccountHookFactory = (deps) => (params) => {
-    const swrRes = useSWR("web3/useAccount", () => {
-      console.log(deps);
-      console.log(params);
-      // making request to get data
-      return "Test User"
-    })
+export const hookFactory : AccountHookFactory = ({provider, ethereum}) => () => {
+    const swrRes = useSWR(
+      provider ? "web3/useAccount" : null,
+     async () => {
+        const accounts = await provider!.listAccounts();
+        console.log("accounts:::::: " + JSON.stringify(accounts));
+        const account = accounts[0];
+
+        console.log("accounts::::::-" + JSON.stringify(account));
   
-    return swrRes;
+        if (!account) {
+          throw ("Cannot retreive account! Please, connect to web3 wallet.");
+        }
+        return account;
+      }, {
+        revalidateOnFocus : false
+      }
+    )
+
+    useEffect(() => {
+      ethereum?.on("accountsChanged", handleAccountsChanged);
+      return () => {
+        ethereum?.removeListener("accountsChanged", handleAccountsChanged);
+      }
+    })
+
+    const handleAccountsChanged = (...args: unknown[]) => {
+      const accounts = args[0] as string[];
+      if (accounts.length === 0) {
+        console.error("Please, connect to Web3 wallet");
+      } else if (accounts[0] !== swrRes.data) {
+        swrRes.mutate(accounts[0])
+      }
+    }
+
+    const connect = async () => {
+      try {
+        ethereum?.request({method: "eth_requestAccounts"});
+      } catch(e) {
+        console.error(e);
+      }
+    }
+  
+    return {
+      ...swrRes,
+      connect
+    }
   }
   
