998b749af3a7e0b3e79416d0cd17eaaf4f23c70c
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-10-25 22:11:45 -0500

    [ Reload on network change ]

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,6 +1,11 @@
 import { createContext, FunctionComponent, useContext, useEffect, useState } from "react"
 import { createDefaultState, createWeb3State, loadContract, Web3State } from "./utils";
 import { ethers } from "ethers";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+function pageReload() {
+  window.location.reload();
+}
 
 const Web3Context = createContext<Web3State>(createDefaultState());
 
@@ -12,15 +17,16 @@ const Web3Provider: FunctionComponent = ({children}) => {
       try {
         const provider = new ethers.providers.Web3Provider(window.ethereum as any);
         const contract =  await loadContract("NftMarket", provider);
-  
+
+        setGlobalListeners(window.ethereum);
         setWeb3Api(createWeb3State({
-          ethereum : window.ethereum,
+          ethereum: window.ethereum,
           provider,
           contract,
-          isLoading : false
+          isLoading: false
         }))
-      } catch (e: any) {
-        console.error(e.message);
+      } catch(e: any) {
+        console.error("Please, install web3 wallet");
         setWeb3Api((api) => createWeb3State({
           ...api as any,
           isLoading: false,
@@ -29,7 +35,16 @@ const Web3Provider: FunctionComponent = ({children}) => {
     }
 
     initWeb3();
+    return () => removeGlobalListeners(window.ethereum);
   }, [])
+
+  const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+    ethereum.on("chainChanged", pageReload);
+  }
+
+  const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+    ethereum.removeListener("chainChanged", pageReload);
+  }
 
   return (
     <Web3Context.Provider value={web3Api}>
@@ -43,7 +58,7 @@ export function useWeb3() {
 }
 
 export function useHooks() {
-  const {hooks} = useWeb3();
+  const { hooks } = useWeb3();
   return hooks;
 }
 
