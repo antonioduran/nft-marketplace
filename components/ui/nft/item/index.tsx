453969342a6688c003b311171008f3eb2597ddd2
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-11-18 21:58:16 -0600

    [ Display Nfts ]

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+
 import { FunctionComponent } from "react";
-import { NftMeta } from "../../../../types/nft";
+import { NftMeta, Nft } from "../../../../types/nft";
 
 type NftItemProps = {
-  item : NftMeta
+  item: Nft;
 }
 
 const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
@@ -12,7 +13,7 @@ const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
       <div className="flex-shrink-0">
         <img
           className={`h-full w-full object-cover`}
-          src= {item.image}
+          src={item.meta.image}
           alt="New NFT"
         />
       </div>
@@ -22,8 +23,8 @@ const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
             Creatures NFT
           </p>
           <div className="block mt-2">
-            <p className="text-xl font-semibold text-gray-900">{item.name}</p>
-            <p className="mt-3 mb-3 text-base text-gray-500">{item.description}</p>
+            <p className="text-xl font-semibold text-gray-900">{item.meta.name}</p>
+            <p className="mt-3 mb-3 text-base text-gray-500">{item.meta.description}</p>
           </div>
         </div>
         <div className="overflow-hidden mb-4">
@@ -34,11 +35,10 @@ const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
                 <div className="flex justify-center items-center">
                   100
                   <img className="h-6" src="/images/small-eth.webp" alt="ether icon"/>
-             
                 </div>
-              </  dd>
+              </dd>
             </div>
-            { item.attributes.map(attribute =>
+            { item.meta.attributes.map(attribute =>
               <div key={attribute.trait_type} className="flex flex-col px-4 pt-4">
                 <dt className="order-2 text-sm font-medium text-gray-500">
                   {attribute.trait_type}
@@ -70,4 +70,3 @@ const NftItem: FunctionComponent<NftItemProps> = ({item}) => {
 }
 
 export default NftItem;
-
