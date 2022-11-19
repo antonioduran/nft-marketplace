6c0f718e0abc8a9c91a0bf0754f179066a79e193
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-11-19 11:51:33 -0600

    [ Buy Nft in Hook ]

--- components/hooks/web3/useListedNfts.ts
+++ components/hooks/web3/useListedNfts.ts
@@ -4,7 +4,9 @@ import { Nft } from "@_types/nft";
 import { ethers } from "ethers";
 import useSWR from "swr";
 
-type UseListedNftsResponse = {}
+type UseListedNftsResponse = {
+  buyNft: (token: number, value: number) => Promise<void>
+}
 type ListedNftsHookFactory = CryptoHookFactory<Nft[], UseListedNftsResponse>
 
 export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>
@@ -30,12 +32,29 @@ export const hookFactory: ListedNftsHookFactory = ({contract}) => () => {
           meta
         })
       }
-
+      
       return nfts;
     }
   )
+
+  const buyNft = async (tokenId: number, value: number) => {
+    try {
+      await contract?.buyNft(
+        tokenId, {
+          value: ethers.utils.parseEther(value.toString())
+        }
+      )
+
+      alert("You have bought Nft. See profile page.")
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  }
+
+
   return {
     ...swr,
+    buyNft,
     data: data || [],
   };
 }
