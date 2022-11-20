cc9cf710c5f049a3bd1b6a229e0cc3885d5548d5
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2022-11-20 10:55:39 -0600

    [  Image upload endpoint ]

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -38,7 +38,7 @@ const NftCreate: NextPage = () => {
   }
 
   const handleImage = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) {
+    if (!e.target.files || e.target.files.length === 0) {
       console.error("Select a file");
       return;
     }
@@ -49,7 +49,13 @@ const NftCreate: NextPage = () => {
     
     try {
       const {signedData, account} = await getSignedData();
-
+      await axios.post("/api/verify-image", {
+        address: account,
+        signature: signedData,
+        bytes,
+        contentType: file.type,
+        fileName: file.name.replace(/\.[^/.]+$/, "")
+      });
     } catch(e: any) {
       console.error(e.message);
     }
